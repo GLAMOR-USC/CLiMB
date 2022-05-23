000f12d1613d52b2f1e110f75c339c3506be3551
Tejas Srinivasan <tejas.srinivasan95@gmail.com>
2022-05-22 21:42:22 -0700

    Plotting average forgetting, fixed EWC weight

--- src/exp_scripts/continual_learning/vqa_nlvr_snlive_vcr/vilt-ewc.sh
+++ src/exp_scripts/continual_learning/vqa_nlvr_snlive_vcr/vilt-ewc.sh
@@ -6,10 +6,10 @@ python -m train.train_cl --encoder_name vilt \
                         --ordered_cl_tasks vqa,nlvr2,snli-ve,vcr \
                         --cl_algorithm ewc \
                         --ewc_fisher_sample_percentage 0.01 \
-			--ewc_loss_weight 0.01 \
+			--ewc_loss_weight 100.0 \
 			--mcl_data_dir /data/datasets/MCL/ \
                         --do_train \
 			--do_eval \
                         --output_dir /data/experiments/MCL/ \
                         --wandb_project_name vl-cl \
-                        --batch_size 16
+                        --batch_size 64
