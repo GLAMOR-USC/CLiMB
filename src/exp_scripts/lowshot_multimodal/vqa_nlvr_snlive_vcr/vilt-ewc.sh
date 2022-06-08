23f87d343f57370f5eceb7a1f5ddb681d1d4630a
Tejas Srinivasan <tejas.srinivasan95@gmail.com>
2022-06-08 15:38:56 -0700

    More lowshot scripts

---
+++ src/exp_scripts/lowshot_multimodal/vqa_nlvr_snlive_vcr/vilt-ewc.sh
@@ -0,0 +1,13 @@
+export WANDB_API_KEY=8cd0c45d6a9418a2997ec6478116a01c14499820
+export TOKENIZERS_PARALLELISM=false
+
+python -m train.train_lowshot_multimodal --encoder_name vilt \
+                        --pretrained_model_name dandelin/vilt-b32-mlm \
+                        --ordered_cl_tasks vqa,nlvr2,snli-ve,vcr \
+                        --cl_algorithm ewc \
+                        --ewc_fisher_sample_percentage 0.01 \
+			--ewc_loss_weight 100.0 \
+			--mcl_data_dir /data/datasets/MCL/ \
+                        --output_dir /data/experiments/MCL/ \
+                        --wandb_project_name vl-lowshot \
+                        --batch_size 64
